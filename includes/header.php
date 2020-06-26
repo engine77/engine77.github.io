60ce0dd4cb0525ff797d242c02a023c98bee6bf5
Zaur <zaur.1kager@gmail.com>
2020-06-26 19:40:05 +0400

    git2

--- includes/header.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="libs/swiper.min.css">
-<link rel="stylesheet" href="libs/font-awesome-4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/style.css">
